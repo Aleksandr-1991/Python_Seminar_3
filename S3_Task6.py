2700305914a8cf69fb424de65425fe8f5996c494
Aleks37 <lao35180972643335589007>
2024-05-09 19:17:05 +0400

    Remote excess code

--- S3_Task6.py
+++ S3_Task6.py
@@ -1,7 +1,7 @@
 print("\033[H\033[J", end="")
 """ Поменять местами последний эл. с 1ым.    """
 array = [5, 651, 612, 62, 621, 612, 98, 2, 65, 3, 69, 98]
-
+print(array)
 # temp = array[0]
 # array[0] = array[-1]
 # array[-1] = temp
@@ -14,9 +14,3 @@ array.insert(0, last)
 array.append(first)
 
 print(array)
-list_1 = [1, 2, 3, 4, 5]
-k = 6
-
-list_of_difference = 0
-for el in list_1:
-    list_of_difference.append
